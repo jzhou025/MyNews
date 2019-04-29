39a5858de54c89d604d0431e8a26ac9907cbab98
jzhou025 <jingyuzhou77@gmail.com>
2019-04-28 23:44:14 -0700

    implement SavedNewsDetailedFragment and AuthorViewModel

--- app/src/main/java/com/jingyu/mynews/save/detail/SavedNewsDetailedFragment.java
+++ app/src/main/java/com/jingyu/mynews/save/detail/SavedNewsDetailedFragment.java
@@ -3,37 +3,64 @@ package com.jingyu.mynews.save.detail;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jingyu.mynews.R;
+import com.jingyu.mynews.common.BaseViewModel;
 import com.jingyu.mynews.common.MyBasicFragment;
+import com.jingyu.mynews.common.Util;
+import com.jingyu.mynews.common.ViewModelAdapter;
+import com.jingyu.mynews.retrofit.response.News;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SavedNewsDetailedFragment extends MyBasicFragment {
 
+    private static final String NEWS = "news";
+    private ViewModelAdapter viewModelAdapter;
 
-    public static SavedNewsDetailedFragment newInstance() {
+    public static SavedNewsDetailedFragment newInstance(News news) {
 
         Bundle args = new Bundle();
-
+        args.putParcelable(NEWS, news);
         SavedNewsDetailedFragment fragment = new SavedNewsDetailedFragment();
         fragment.setArguments(args);
         return fragment;
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_saved_news_detailed, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        viewModelAdapter = new ViewModelAdapter();
+        recyclerView.setAdapter(viewModelAdapter);
+        return view;
+    }
+
+    private void loadNews(News news){
+        List<BaseViewModel> viewModels = new LinkedList<>();
+        if (!Util.isStringEmpty(news.author) || !Util.isStringEmpty(news.time)) {
+            viewModels.add(new AuthorViewModel(news.author, news.time));
+        }
+        viewModelAdapter.addViewModels(viewModels);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        loadNews(getArguments().getParcelable(NEWS));
     }
 
 }
